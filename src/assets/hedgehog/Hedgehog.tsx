84b8bae688ff5468bd3d347e37dc96182a8592a7
Your Name <y.y.oleksander@gmail.com>
2025-10-11 17:40:07 +0200

    1110 bodyHedgehog

--- src/assets/hedgehog/Hedgehog.tsx
+++ src/assets/hedgehog/Hedgehog.tsx
@@ -7,7 +7,21 @@ function Hedgehog(props: hedgehogProps) {
   for (let i = 0; i < 10; i++) {
     frontPoints.push(new Vector2((Math.sin(i * 0.2) * 8 + 8) / 25, (i + 6) / 15))
   }
+
+  // const backPoints = []
+  // for (let i = 0; i < 10; i++) {
+  //   backPoints.push(new Vector2((Math.sin(i * 0.2) * 8 + 8) / 25, (i * 0.9 - 21) / 15))
+  // }
   const backPoints = []
+
+  // Полукруглая заглушка (3 точки)
+  const r = (Math.sin(0 * 0.2) * 8 + 8) / 65
+  const y0 = (0 * 0.9 - 21) / 15
+  backPoints.push(new Vector2(r, y0)) // край
+  backPoints.push(new Vector2(r * 0.7, y0 - r * 0.3)) // середина дуги
+  backPoints.push(new Vector2(0, y0 - r)) // центр (закрывает дырку)
+
+  // Основные точки
   for (let i = 0; i < 10; i++) {
     backPoints.push(new Vector2((Math.sin(i * 0.2) * 8 + 8) / 25, (i * 0.9 - 21) / 15))
   }
@@ -21,13 +35,18 @@ function Hedgehog(props: hedgehogProps) {
       ]}
     >
       <mesh
+        position={[
+          line === 'Y' ? 0 : -0.2 * direction[index],
+          line === 'Y' ? -0.2 * direction[index] : 0,
+          0,
+        ]}
         rotation={[
           0,
           0,
           line === 'X' ? direction[index] * -1.57 : ((direction[index] - 1) / 2) * 3.14,
         ]}
       >
-        <coneGeometry args={[0.4, 1, 15, 1, false, 4.7, 3.14]} />
+        <coneGeometry args={[0.4, 0.5, 3, 1, false, 4.7, 3.14]} />
         <meshStandardMaterial color='#A18E74' />
       </mesh>
       <mesh
@@ -41,6 +60,7 @@ function Hedgehog(props: hedgehogProps) {
         <meshStandardMaterial color={'#5B586A'} />
       </mesh>
       <mesh
+        position={[0, 0, 0]}
         rotation={[
           0,
           0,
@@ -50,17 +70,14 @@ function Hedgehog(props: hedgehogProps) {
         <latheGeometry args={[backPoints]} />
         <meshStandardMaterial color={'#5B586A'} />
       </mesh>
+
       <mesh
         position={[
-          line === 'Y' ? 0 : -1 * direction[index],
-          line === 'Y' ? -1 * direction[index] : 0,
-          0,
+          line === 'X' ? direction[index] * -0.03 : 0,
+          line === 'Y' ? direction[index] * -0.03 : 0,
+          0.05,
         ]}
       >
-        <sphereGeometry args={[0.52, 5]} />
-        <meshStandardMaterial color={'#5B586A'} />
-      </mesh>
-      <mesh position={[line === 'X' ? direction[index] * 0.1 : 0, 0, 0.2]}>
         <sphereGeometry args={[0.1, 5]} />
         <meshStandardMaterial color={'#5B586A'} />
       </mesh>
